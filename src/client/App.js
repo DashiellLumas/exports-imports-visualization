92d643dc41de13230004b6d9071a19d369e0037a
DashiellLumas <dashiell.lumas@gmail.com>
2018-09-02 21:32:27 -0400

    added topography background

--- src/client/App.js
+++ src/client/App.js
@@ -56,6 +56,19 @@ export default class App extends Component {
         targetCountry: targetCountry
       })
     })
+    .catch((error) => {
+      if(error.res){
+        console.log(error.res.data);
+        console.log(error.res.status);
+        console.log(error.res.headers);
+      }
+      else if(error.request){
+        console.log(error.request)
+
+      } else {
+        console.log('Error', error.message);
+      }
+    })
   }
 
   handleMouseLeave() {
@@ -65,10 +78,14 @@ export default class App extends Component {
   render() {
     return (
       <div style={wrapperStyles}>
-        <h1>USA Exports Import by Country</h1>
-        <div>{this.state.totalExports}</div>
-        <div>{this.state.totalImports}</div>
-        <div>{this.state.targetCountry}</div>
+        <h1 className="center-text">USA Exports & Imports</h1>
+        <div className="speech-bubble">
+          <div>Export Value: ${this.state.totalExports}</div>
+          <div>Import Value: ${this.state.totalImports}</div>
+          <div>Country: {this.state.targetCountry}</div>
+        </div>
+
+
         <ComposableMap projectionConfig={{
           scale: 205
         }} width={980} height={551} style={{
@@ -77,17 +94,17 @@ export default class App extends Component {
         }}>
           <ZoomableGroup center={[0, 20]} disablePanning>
             <Geographies geography="/static/world-50m.json">
-              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (<Geography key={i} geography={geography} projection={projection} id={geography.properties.name} onClick={this.handleMouseClick} onMouseMove={this.handleMouseMove} onMouseLeave={this.handleMouseLeave} style={{
+              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (<Geography key={i} geography={geography} projection={projection} className="country" id={geography.properties.name} onClick={this.handleMouseClick} onMouseMove={this.handleMouseMove} onMouseLeave={this.handleMouseLeave} style={{
                 default: {
-                  fill: "#ECEFF1",
-                  stroke: "#607D8B",
-                  strokeWidth: 0.75,
+                  fill: "green",
+                  stroke: "black",
+                  strokeWidth: 1.25,
                   outline: "none"
                 },
                 hover: {
-                  fill: "#607D8B",
-                  stroke: "#607D8B",
-                  strokeWidth: 0.75,
+                  fill: "rgba(15,112,1,.02)",
+                  stroke: "black",
+                  strokeWidth: 1.25,
                   outline: "none"
                 },
                 pressed: {
